b8abbbe06908399acbc28dffb50bfebe7d415fbf
guy187 <guylherme699@gmail.com>
2024-04-11 19:26:11 -0300

    Projeto adicionado

---
+++ Etec-app/resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Etec</title>
+    <link rel="stylesheet" href="/CSS/styles.css">
+</head>
+<body>
+<Header>
+
+<div class="container">
+    <nav>
+        <a href="">0</a>
+
+        <ul class="ul">
+            <a href="/noticias">Noticias</a>
+            <a href="/contact">Contato</a>
+            <a href="/quem_somos">Quem Somos</a>
+            <a href=""></a>
+            p*10000
+        </ul>
+    </nav>
+</div>
+
+</Header>
+    
+</body>
+</html>
